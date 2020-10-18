ea5f462318edfee3ce5589f3fb452a017d8394e6
7070587 <7070587@gmail.com>
2020-10-18 17:53:51 +0800

    3 The Rosa  | update animate

--- 3 The Rosa/script/main.js
+++ 3 The Rosa/script/main.js
@@ -8,3 +8,34 @@ menuToggle.addEventListener("click", showNav);
 function showNav() {
   body.classList.toggle("open");
 }
+
+// use 套件 ScrollReveal.js or AOS - Animate On Scroll library
+// animate scroll reveal
+window.sr = ScrollReveal();
+sr.reveal(".animate__left", {
+  origin: "left",
+  duration: 1000,
+  distance: "25rem",
+  delay: 300,
+});
+
+sr.reveal(".animate__right", {
+  origin: "right",
+  duration: 1000,
+  distance: "25rem",
+  delay: 600,
+});
+
+sr.reveal(".animate__top", {
+  origin: "top",
+  duration: 1000,
+  distance: "25rem",
+  delay: 600,
+});
+
+sr.reveal(".animate__bottom", {
+  origin: "bottom",
+  duration: 1000,
+  distance: "25rem",
+  delay: 600,
+});
